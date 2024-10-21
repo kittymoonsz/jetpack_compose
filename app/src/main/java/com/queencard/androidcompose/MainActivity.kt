e04e4600683c3b699589211b03a459979df56468
kittymoonsz <iamkittymoon@gmail.com>
2024-10-21 17:34:01 -0300

    task manager challenge

--- app/src/main/java/com/queencard/androidcompose/MainActivity.kt
+++ app/src/main/java/com/queencard/androidcompose/MainActivity.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.queencard.androidcompose.ui.theme.AndroidcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +33,6 @@ class MainActivity : ComponentActivity() {
                 color = MaterialTheme.colorScheme.background
             ) {
                 JetpackTutorial(
-                    stringResource(R.string.jetpack_title),
                     stringResource(R.string.jetpack_text_one),
                     stringResource(R.string.jetpack_text_two)
                 )
@@ -37,28 +42,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetpackTutorial(title: String, firstText: String, secondText: String) {
-    val image = painterResource(R.drawable.bg_compose_background)
+fun JetpackTutorial(firstText: String, secondText: String) {
+    val image = painterResource(R.drawable.ic_task_completed)
     Column(
-        Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
     ) {
         Image(
             painter = image,
-            contentDescription = null
-        )
-        Text(
-            text = title,
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(8.dp)
-        )
-        Text(
-            text = firstText,
-            modifier = Modifier
-                .padding(8.dp)
+            contentDescription = null,
+
         )
         Text(
             text = secondText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = firstText,
+            fontSize = 16.sp,
             modifier = Modifier
                 .padding(8.dp)
         )
@@ -71,7 +79,6 @@ fun JetpackTutorial(title: String, firstText: String, secondText: String) {
 @Composable
 fun JetpackTutorialPreview() {
     JetpackTutorial(
-        stringResource(R.string.jetpack_title),
         stringResource(R.string.jetpack_text_one),
         stringResource(R.string.jetpack_text_two)
     )
