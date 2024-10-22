43d3adb23e8a44b9cb14bee0199140bd679cb7b2
kittymoonsz <iamkittymoon@gmail.com>
2024-10-22 00:18:04 -0300

    quadrant challenge

--- app/src/main/java/com/queencard/androidcompose/MainActivity.kt
+++ app/src/main/java/com/queencard/androidcompose/MainActivity.kt
@@ -3,10 +3,10 @@ package com.queencard.androidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,13 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.queencard.androidcompose.ui.theme.AndroidcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,54 +31,84 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                JetpackTutorial(
-                    stringResource(R.string.jetpack_text_one),
-                    stringResource(R.string.jetpack_text_two)
-                )
+                QuadrantApp()
             }
+        }
+    }
+}
+@Composable
+fun QuadrantApp() {
+    Column(
+        Modifier.fillMaxWidth()
+    ) {
+        Row(
+            Modifier.weight(1f)
+        ) {
+            ComposeInfo(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+
+            )
+            ComposeInfo(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            Modifier.weight(1f)
+        ) {
+            ComposeInfo(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposeInfo(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
 
 @Composable
-fun JetpackTutorial(firstText: String, secondText: String) {
-    val image = painterResource(R.drawable.ic_task_completed)
+fun ComposeInfo(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
+        modifier = modifier
+            .background(backgroundColor)
+            .padding(16.dp)
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-
+        verticalArrangement = Arrangement.Center,
     ) {
-        Image(
-            painter = image,
-            contentDescription = null,
-
+        Text(
+            text = title,
+            modifier = Modifier
+                .padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
         )
         Text(
-            text = secondText,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp,
-            modifier = Modifier
-                .padding(top = 24.dp, bottom = 8.dp)
-        )
-        Text(
-            text = firstText,
-            fontSize = 16.sp,
-            modifier = Modifier
-                .padding(8.dp)
+            text = description,
+            textAlign = TextAlign.Justify
         )
     }
+
 }
 
-
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun JetpackTutorialPreview() {
-    JetpackTutorial(
-        stringResource(R.string.jetpack_text_one),
-        stringResource(R.string.jetpack_text_two)
-    )
+fun ComposeQuadrantPreview() {
+    QuadrantApp()
 }
+
